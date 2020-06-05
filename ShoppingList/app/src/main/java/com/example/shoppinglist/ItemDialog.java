13105e22e4e2fc92cd3a26ac7241a152716577a6
ToraMullings316 <tora.mullings@stonybrook.edu>
2020-06-05 15:26:05 -0400

    Added a dialog

---
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ItemDialog.java
@@ -0,0 +1,47 @@
+package com.example.shoppinglist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.DialogFragment;
+
+public class ItemDialog extends DialogFragment {
+
+    Item selected_item;
+    public interface SaveItemListener {
+        void didFinishItemDialog(Item finishedItem);
+    }
+
+    public ItemDialog() {}  // Empty constructor
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.edit_item, container);
+
+        getDialog().setTitle("Edit Item");
+
+        final EditText edit_name = view.findViewById(R.id.editTextName);
+        final EditText edit_category = view.findViewById(R.id.editTextCategory);
+        Button button_save = view.findViewById(R.id.buttonSave);
+
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                selected_item.setCategory(edit_name.toString());
+                selected_item.setCategory(edit_category.toString());
+                saveItem(selected_item);
+            }
+        });
+        return view;
+    }
+    private void saveItem(Item item) {
+        SaveItemListener activity = (SaveItemListener) getActivity();
+        activity.didFinishItemDialog(item);
+        getDialog().dismiss();
+    }
+}
--- ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -2,7 +2,20 @@ package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initAddContactButton();
+        ShoppingListDataSource ds = new ShoppingListDataSource(this);
+        ArrayList<String> names;
+
+        try {
+            ds.open();
+            names = ds.getItemName();
+            ds.close();
+            RecyclerView itemList = findViewById(R.id.rvItems);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            itemList.setLayoutManager(layoutManager);
+            com.example.shoppinglist.ItemAdapter itemAdapter = new com.example.shoppinglist.ItemAdapter(names);
+            itemList.setAdapter(itemAdapter);
+        }
+        catch(Exception e) {
+            Toast.makeText(this, "Error retrieving contacts", Toast.LENGTH_LONG).show();
+        }
+
     }
+
+    private void initAddContactButton() {
+        Button newContact = findViewById(R.id.buttonAddItem);
+        newContact.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                FragmentManager fm = getSupportFragmentManager();
+                ItemDialog itemDialog = new ItemDialog();
+                itemDialog.show(fm, "Add Item");
+            }
+        });
+    }
+
+    public class ItemAdapter extends RecyclerView.Adapter {
+        private ArrayList<String> itemData;
+
+        public class ItemViewHolder extends RecyclerView.ViewHolder {
+            public TextView textViewItem;
+            public ItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+                textViewItem = itemView.findViewById(R.id.textViewItemName);
+            }
+
+            public TextView getItemTextView() { return textViewItem;}
+        }
+
+        public ItemAdapter(ArrayList<String> arrayList) { itemData = arrayList;}
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new ItemViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            com.example.shoppinglist.ItemAdapter.ItemViewHolder ivh = (com.example.shoppinglist.ItemAdapter.ItemViewHolder) holder;
+            ivh.getItemTextView().setText(itemData.get(position));
+        }
+
+        @Override
+        public int getItemCount() { return itemData.size(); }
+    }
+
+
+
 }
